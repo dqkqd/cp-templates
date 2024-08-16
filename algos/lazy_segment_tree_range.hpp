f934787ad64d596ed14ef5fe8959a252de85398f
Khanh Duong Quoc <dqkqdlot@gmail.com>
2024-08-16 10:50:13 +0900

    feat: add lazy segment tree with range

---
+++ algos/lazy_segment_tree_range.hpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+
+template <typename T>
+concept IsOp = requires(T t, const T& o) {
+    { t.apply(o) } -> std::same_as<void>;
+    { T() };
+};
+
+template <typename T, typename U>
+concept IsNode =
+    requires(T t, const T& a, const T& b, const U& o, int l, int r) {
+        { t.apply(o, l, r) } -> std::same_as<void>;
+        { a + b } -> std::same_as<T>;
+        { T() };
+    };
+
+template <typename Node, typename Op>
+    requires IsOp<Op> && IsNode<Node, Op>
+struct LazySegmentTree {
+    int n;
+    std::vector<Node> tree;
+    std::vector<Op> lazy;
+
+    LazySegmentTree(int n_)
+        : n(n_),
+          tree(4 << std::__lg(n), Node()),
+          lazy(4 << std::__lg(n), Op()) {}
+
+    LazySegmentTree(std::vector<Node> a) : LazySegmentTree(a.size()) {
+        std::function<void(int, int, int)> build = [&](int v, int l, int r) {
+            if (l == r - 1) {
+                tree[v] = a[l];
+            } else {
+                int m = (l + r) / 2;
+                build(v * 2, l, m);
+                build(v * 2 + 1, m, r);
+                tree[v] = tree[v * 2] + tree[v * 2 + 1];
+            }
+        };
+        build(1, 0, n);
+    }
+
+    void apply(int v, const Op& op, int l, int r) {
+        tree[v].apply(op, l, r);
+        lazy[v].apply(op);
+    }
+
+    void push(int v, int l, int r) {
+        int m = (l + r) / 2;
+        apply(v * 2, lazy[v], l, m);
+        apply(v * 2 + 1, lazy[v], m, r);
+        lazy[v] = Op();
+    }
+
+    Node get(int v, int tl, int tr, int l, int r) {
+        if (l >= tr || r <= tl) {
+            return Node();
+        }
+        if (tl >= l && tr <= r) {
+            return tree[v];
+        }
+        push(v, tl, tr);
+        int tm = (tl + tr) / 2;
+        return get(v * 2, tl, tm, l, r) + get(v * 2 + 1, tm, tr, l, r);
+    }
+
+    Node get(int l, int r) { return get(1, 0, n, l, r); }
+
+    void set(int v, int tl, int tr, int l, int r, const Op& op) {
+        if (l >= tr || r <= tl) {
+            return;
+        }
+        if (tl >= l && tr <= r) {
+            apply(v, op, tl, tr);
+            return;
+        }
+        push(v, tl, tr);
+        int tm = (tl + tr) / 2;
+        set(v * 2, tl, tm, l, r, op);
+        set(v * 2 + 1, tm, tr, l, r, op);
+        tree[v] = tree[v * 2] + tree[v * 2 + 1];
+    }
+
+    void set(int l, int r, const Op& op) { set(1, 0, n, l, r, op); }
+};
+
+struct Op {
+    void apply([[maybe_unused]] const Op& op) {
+        throw std::logic_error("Not Implemented");
+    }
+};
+
+struct Node {
+    void apply([[maybe_unused]] const Op& op, [[maybe_unused]] int l,
+               [[maybe_unused]] int r) {
+        throw std::logic_error("Not Implemented");
+    }
+    friend Node operator+([[maybe_unused]] const Node& lhs,
+                          [[maybe_unused]] const Node& rhs) {
+        throw std::logic_error("Not Implemented");
+    }
+};
---
+++ tests/test_lazy_segment_tree_range.cpp
@@ -0,0 +1,68 @@
+#include <gtest/gtest.h>
+
+#include <lazy_segment_tree_range.hpp>
+
+struct OpRA {
+    int value = 0;
+    void apply(const OpRA& op) { value += op.value; }
+};
+
+struct NodeRA {
+    int value = 0;
+    void apply(const OpRA& op, int l, int r) { value += op.value * (r - l); }
+    friend NodeRA operator+(const NodeRA& lhs, const NodeRA& rhs) {
+        return NodeRA{lhs.value + rhs.value};
+    }
+};
+
+TEST(LazySegmentTreeRange, AddAndSum) {
+    std::vector<NodeRA> a = {NodeRA{2}, NodeRA{4}, NodeRA{6}};
+    LazySegmentTree<NodeRA, OpRA> st(a);
+
+    st.set(0, 3, OpRA{5});
+    EXPECT_EQ(st.get(0, 1).value, 7);
+    EXPECT_EQ(st.get(0, 2).value, 16);
+    EXPECT_EQ(st.get(0, 3).value, 27);
+
+    st.set(1, 3, OpRA{1});
+    EXPECT_EQ(st.get(0, 1).value, 7);
+    EXPECT_EQ(st.get(1, 2).value, 10);
+    EXPECT_EQ(st.get(1, 3).value, 22);
+}
+
+struct OpRB {
+    std::optional<int> value = {};
+    void apply(const OpRB& op) {
+        if (op.value.has_value()) {
+            value = op.value;
+        }
+    }
+};
+
+struct NodeRB {
+    int value = 0;
+    void apply(const OpRB& op, int l, int r) {
+        if (op.value.has_value()) {
+            value = op.value.value() * (r - l);
+        }
+    }
+    friend NodeRB operator+(const NodeRB& lhs, const NodeRB& rhs) {
+        return NodeRB{lhs.value + rhs.value};
+    }
+};
+
+TEST(LazySegmentTreeRange, AssignAndSum) {
+    std::vector<NodeRB> a = {NodeRB{2}, NodeRB{4}, NodeRB{6}};
+    LazySegmentTree<NodeRB, OpRB> st(a);
+
+    st.set(0, 2, OpRB{3});
+
+    EXPECT_EQ(st.get(0, 1).value, 3);
+    EXPECT_EQ(st.get(0, 2).value, 6);
+    EXPECT_EQ(st.get(0, 3).value, 12);
+
+    st.set(1, 3, OpRB{1});
+    EXPECT_EQ(st.get(0, 1).value, 3);
+    EXPECT_EQ(st.get(1, 2).value, 1);
+    EXPECT_EQ(st.get(1, 3).value, 2);
+}
